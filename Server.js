ec58b68301167d085353b9d7676d2f5f9d6870f3
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-04 19:13:03 +0100

    Updated server.js

--- Server.js
+++ Server.js
@@ -1,26 +1,13 @@
- require("dotenv").config() //Loads env
- 
- const express = require('express')
+require('dotenv').config()
+
+import express from 'express'
+
 const app = express()
-const port = process.env.PORT;
-
-app.use(express.json()); // ADDED EXPRESS MIDDLEWARE WHICH IS EXPRESS.JSON
-
-app.post('/echo', (req, res) => {
-  console.log("req.body ")
-  res.json({echoed: req.body });   // REQ.BODY IS NOW AVAILABLE
-});
-
-app.get('/user/:id', (req, res) => {
-  const id = req.params.id
-  console.log(id)
-  res.send(id)
-});
 
 app.get('/', (req, res) => {
-  res.send(" Hello World! ")
-});
+  res.send('Hello World')
+})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
 })
