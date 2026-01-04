9bd2ad325b80659ed57243042750f16e580571b3
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-04 05:11:33 +0100

    updated server.js

--- Server.js
+++ Server.js
@@ -1,9 +1,18 @@
+require("dotenv").config() //Loads env //LOADS ENV
+
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT ;// MOVED PORT TO DOTENV
+
+app.use(express.json())
+
+app.post('/user', (req, res) => {
+  console.log("req.body ")
+  res.json({echoed: req.body });   // REQ.BODY IS NOW AVAILABLE
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello everybody gerge!')
 })
 
 app.listen(port, () => {
