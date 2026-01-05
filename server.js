da6e6acfc3408735136354470b520caca6d61a70
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-05 02:45:28 +0100

    Updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 require("dotenv").config();
 
+
 const express = require('express')
 const app = express();
 const port = process.env.PORT
 
+app.use(express.json())
+
+app.use(express.json()); // ADDED EXPRESS MIDDLEWARE WHICH IS EXPRESS.JSON
+
+app.post('/user', (req, res) => {
+  console.log("req.body ")
+  res.json({echoed: req.body });   // REQ.BODY IS NOW AVAILABLE
+});
+
 app.get('/hello', (req, res) => {
-  res.send('Hello Fiyinfoluwa! I am worried!')
+  res.send('Hello Fiyinfoluwa, I am worried that my code aint working!')
 });
 
 app.listen(port, () => {
