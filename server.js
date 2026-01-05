4892fbb5d44479c07d8504305bb1c304d0840197
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-01-05 02:33:19 +0100

    Updated server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 
 const express = require('express')
-const app = express()
+const app = express();
 const port = process.env.PORT
 
 app.get('/hello', (req, res) => {
-  res.send('Hello Fiyinfoluwa! I AM WORRIED ')
+  res.send('Hello Fiyinfoluwa! I am worried!')
 });
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
+
