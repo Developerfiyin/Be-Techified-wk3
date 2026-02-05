577c705d76c5781ab45252eeba20098723993f45
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-05 15:52:53 +0100

    Updated global handler file for error handler

--- global-handler.js
+++ global-handler.js
@@ -1,1 +1,7 @@
-const
+const globalHandler = (req, res) => {
+    console.log(err)
+    const status =  err.status || 500
+    res.status( status).json({error: error.message})
+}
+
+module.error= globalHandler;
