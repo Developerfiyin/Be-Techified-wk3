1a55096e7130dd48ad196ae03b19aae66e90a5d1
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-05 15:04:36 +0100

    Updated  validation.js

--- validation.js
+++ validation.js
@@ -8,7 +8,8 @@ completed : Joi.boolen().default(false)
 }) 
 const {error } = schema.validate(req.body);
 if (error) {
-    return
+    return res.status(400).json({error : error.details[0].message})
 }
 }
  
+module.exports = ValidaTodo
